20687fe2029676bd610930b7aeef218ee05271ba
bhavik gilbert <83555685+Bhavik-Gilbert@users.noreply.github.com>
2024-12-07 13:50:10 +0000

    Update day7.py

--- 2024/day7.py
+++ 2024/day7.py
@@ -1,15 +1,5 @@
 from inputs.day7 import INPUT_STR
 
-# INPUT_STR = """190: 10 19
-# 3267: 81 40 27
-# 83: 17 5
-# 156: 15 6
-# 7290: 6 8 6 15
-# 161011: 16 10 13
-# 192: 17 8 14
-# 21037: 9 7 18 13
-# 292: 11 6 16 20"""
-
 VALID_OPERATORS_1 = ["+", "*"]
 VALID_OPERATORS_2 = VALID_OPERATORS_1 + ["||"]
 
